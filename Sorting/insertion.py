a7aecaa605a2041579bd2e25bc1fad13d966ddd0
Canaan 🐈 <reakak1@gmail.com>
2023-07-11 14:06:25 +0300

    Insertion

--- Sorting/insertion.py
+++ Sorting/insertion.py
@@ -2,9 +2,12 @@ from typing import List
 
 
 def insertion_sort(li : List) -> List:
-
     for i in range(1, len(li)):
         t = li[i]
         j = i -1
+        # reversing the < to > makes it sort in reverse
         while t < li[j] and j > -1:
             li[j + 1] = li[j] 
+            li[j] = t
+            j -= 1
+    return li
